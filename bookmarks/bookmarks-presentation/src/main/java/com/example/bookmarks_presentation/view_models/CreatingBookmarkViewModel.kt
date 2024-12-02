415ed263090a761e2b878ff48bf0abd4a6b56107
Olexandr.pl <olexandr.pl.04@gmail.com>
2024-12-02 19:37:06 +0200

    Main: user can add information in bookmark, besides url of dialog with chat gpt

--- bookmarks/bookmarks-presentation/src/main/java/com/example/bookmarks_presentation/view_models/CreatingBookmarkViewModel.kt
+++ bookmarks/bookmarks-presentation/src/main/java/com/example/bookmarks_presentation/view_models/CreatingBookmarkViewModel.kt
@@ -1,5 +1,11 @@
 package com.example.bookmarks_presentation.view_models
 
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.webkit.MimeTypeMap
+import android.widget.Toast
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
@@ -15,22 +21,30 @@ import kotlinx.coroutines.launch
 import org.koin.android.annotation.KoinViewModel
 
 @KoinViewModel
-class CreatingBookmarkViewModel(private val bookmarksUseCase:BookmarksUseCase):ViewModel() {
+class CreatingBookmarkViewModel(private val bookmarksUseCase: BookmarksUseCase) : ViewModel() {
 
     private val _tittleOfBookmark = mutableStateOf("")
-    val tittle:State<String> = _tittleOfBookmark
+    val tittle: State<String> = _tittleOfBookmark
+
+    private val _contentUrl = mutableStateOf<String?>(null)
+    val contentUrl: State<String?> = _contentUrl
+
+    private val _fileUri = mutableStateOf<String?>(null)
+    val fileUri: State<String?> = _fileUri
 
 
-    private fun createBookmark(bookmark: Bookmark){
-        bookmarksUseCase.createBookmark(bookmark).onEach {result->
-            when(result){
+    private fun createBookmark(bookmark: Bookmark) {
+        bookmarksUseCase.createBookmark(bookmark).onEach { result ->
+            when (result) {
                 is Responses.Error -> {
 
                 }
+
                 is Responses.Loading -> {
 
                 }
-                is Responses.Success-> {
+
+                is Responses.Success -> {
 
                 }
             }
@@ -39,15 +53,18 @@ class CreatingBookmarkViewModel(private val bookmarksUseCase:BookmarksUseCase):V
     }
 
 
-    fun oEvent(events: CreatingBookmarkEvents){
-        when(events){
+    fun oEvent(events: CreatingBookmarkEvents) {
+        when (events) {
             is CreatingBookmarkEvents.GetBookmarkById -> {
 
             }
+
             CreatingBookmarkEvents.SaveBookmark -> {
                 viewModelScope.launch {
                     val bookmark = Bookmark(
-                        tittle = _tittleOfBookmark.value
+                        tittle = _tittleOfBookmark.value,
+                        fileUri = _fileUri.value,
+                        url = _contentUrl.value
                     )
                     createBookmark(bookmark)
                 }
@@ -56,6 +73,21 @@ class CreatingBookmarkViewModel(private val bookmarksUseCase:BookmarksUseCase):V
             is CreatingBookmarkEvents.UpdateTittleOfBookmark -> {
                 _tittleOfBookmark.value = events.tittle
             }
+
+            is CreatingBookmarkEvents.AddFile -> {
+                _fileUri.value = events.uri
+            }
+            is CreatingBookmarkEvents.AddLink -> {
+                _contentUrl.value = events.url
+            }
+            CreatingBookmarkEvents.CancelFile -> {
+                _fileUri.value = null
+            }
+            CreatingBookmarkEvents.CancelLink -> {
+                _contentUrl.value = null
+            }
+            is CreatingBookmarkEvents.OpenFile -> TODO()
+            is CreatingBookmarkEvents.OpenLink -> TODO()
         }
     }
 
