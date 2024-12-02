86bd101ed48012dccb292c2e810e48bc1151d5cf
Olexandr.pl <olexandr.pl.04@gmail.com>
2024-12-03 01:10:31 +0200

    Main: added all bookmarksScreen and getting all bookmarks logic

--- bookmarks/bookmarks-presentation/src/main/java/com/example/bookmarks_presentation/events/main_events/UiMainEvents.kt
+++ bookmarks/bookmarks-presentation/src/main/java/com/example/bookmarks_presentation/events/main_events/UiMainEvents.kt
@@ -2,4 +2,5 @@ package com.example.bookmarks_presentation.events.main_events
 
 sealed class UiMainEvents {
     data class NavigateToCreateBookmark(val id:Int?):UiMainEvents()
+    data object NavigateToAllBookMarks:UiMainEvents()
 }
--- bookmarks/bookmarks-presentation/src/main/java/com/example/bookmarks_presentation/ui/AllBookmarksScreen.kt
+++ bookmarks/bookmarks-presentation/src/main/java/com/example/bookmarks_presentation/ui/AllBookmarksScreen.kt
@@ -2,17 +2,27 @@ package com.example.bookmarks_presentation.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.bookmarks_presentation.view_models.AllBookmarksViewModel
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun AllBookmarksScreen(modifier: Modifier = Modifier) {
-
+val viewModel = koinViewModel<AllBookmarksViewModel>()
     Box(modifier.fillMaxSize().background(Color.Green), contentAlignment = Alignment.Center) {
-        Text("lalalal")
+        Column(modifier = Modifier.fillMaxSize()) {
+            viewModel.listOfBookmarks.value?.forEach {
+                Card(modifier = Modifier.height(200.dp)) { Text(it.tittle) }
+            }
+        }
     }
 }
