cec6a842b7557ff22c9b71537d6ea112cbb935a3
Olexandr.pl <olexandr.pl.04@gmail.com>
2024-12-03 20:29:23 +0200

    Main: added ability to update bookmark

--- bookmarks/bookmarks-domain/src/main/java/com/example/bookmarks_domain/interactors/BookmarksRepository.kt
+++ bookmarks/bookmarks-domain/src/main/java/com/example/bookmarks_domain/interactors/BookmarksRepository.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface BookmarksRepository {
     suspend fun createBookmark(bookmark: Bookmark)
+    suspend fun updateBookmark(bookmark: Bookmark)
     suspend fun deleteBookmark(bookmark: Bookmark)
     suspend fun getBookmarkById(id:Int): Flow<Bookmark>
     suspend fun getAllBookmarks():Flow<List<Bookmark>>
