e0954d41a4f054c0318f2f61e080aec4b7f8abd9
Olexandr.pl <olexandr.pl.04@gmail.com>
2024-12-06 18:51:26 +0200

    Main: added exception when user set time in past for reminder

--- tasks/tasks-data/src/main/java/com/aopr/tasks_data/utils/FieldsValidator.kt
+++ tasks/tasks-data/src/main/java/com/aopr/tasks_data/utils/FieldsValidator.kt
@@ -9,7 +9,9 @@ import com.aopr.tasks_data.scheduled_notifucation.scheduleTaskReminder
 import com.aopr.tasks_domain.models.Subtasks
 import com.aopr.tasks_domain.models.Task
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.LocalTime
+import java.time.ZoneId
 
 class FieldsValidator {
 
@@ -27,6 +29,16 @@ class FieldsValidator {
             if (tittle.isBlank()) throw EmptyTittleException()
             if (description.isBlank()) throw EmptyDescriptionException()
             if (dateForToBeDone == null) throw EmptyDescriptionException()
+
+
+            if (date != null && time != null) {
+                val reminderDateTime = LocalDateTime.of(date, time)
+                val currentDateTime = LocalDateTime.now(ZoneId.systemDefault())
+                if (reminderDateTime.isBefore(currentDateTime)) {
+                    throw EmptyTittleException()
+                }
+
+        }
         }
         fun validateSubTask(
             tittle:String,
@@ -36,8 +48,16 @@ class FieldsValidator {
             if (date != null && time == null) throw EmptyTimeForReminderException()
             if (time != null && date == null) throw EmptyDateForReminderException()
             if (tittle.isBlank()) throw EmptyTittleException()
+            if (date != null && time != null) {
+                val reminderDateTime = LocalDateTime.of(date, time)
+                val currentDateTime = LocalDateTime.now(ZoneId.systemDefault())
+                if (reminderDateTime.isBefore(currentDateTime)) {
+                    throw EmptyTittleException()
+                }
+            }
 
         }
+
 
         fun scheduleReminders(task: Task, subtasks: List<Subtasks>?,context: Context) {
             if (task.dateForReminder != null && task.timeForReminder != null) {
